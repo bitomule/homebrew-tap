7bee2b2642e73fe65382c5208eebee519235115b
David Collado <david.collado@wallapop.com>
2020-04-22 09:45:55 +0200

    Publish swiftypods

---
+++ swiftypods.rb
@@ -0,0 +1,11 @@
+class SwiftyPods < Formula
+  desc "A tool to generate CocoaPods podfile's using Swift"
+  homepage "https://github.com/bitomule/SwiftyPods"
+  url "https://github.com/bitomule/SwiftyPods.git",
+      :tag => "0.1.3"
+  head "https://github.com/bitomule/SwiftyPods.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
