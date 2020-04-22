164c0d3eafed39c892c159ea4e923c6b5143b4e6
David Collado <david.collado@wallapop.com>
2020-04-22 16:34:32 +0200

    Fix version typo

--- swiftypods.rb
+++ swiftypods.rb
@@ -2,7 +2,7 @@ class SwiftyPods < Formula
   desc "A tool to generate CocoaPods podfile's using Swift"
   homepage "https://github.com/bitomule/SwiftyPods"
   url "https://github.com/bitomule/SwiftyPods.git",
-      :tag => "0.1.3"
+      :tag => "0.1.0"
   head "https://github.com/bitomule/SwiftyPods.git"
 
   def install
