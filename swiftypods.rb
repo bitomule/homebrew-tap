6da1eed44b1434cd73da670a56cb78e51c8850d4
David <bitomule@gmail.com>
2020-04-24 20:15:06 +0200

    Update DSL version

--- swiftypods.rb
+++ swiftypods.rb
@@ -2,7 +2,7 @@ class Swiftypods < Formula
   desc "SwiftyPods is a command-line tool that allows you to generate your CocoaPods podfile using Swift"
   homepage "https://github.com/bitomule/SwiftyPods"
   url "https://github.com/bitomule/SwiftyPods.git",
-      :tag => "0.1.0"
+      :tag => "0.2.0"
   head "https://github.com/bitomule/SwiftyPods.git"
 
   def install
